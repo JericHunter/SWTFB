7dc639753142250e1ee8de6b871e1d3f6f89f237
JericHunter <jeric.hunterr@gmail.com>
2020-03-23 02:40:47 -0400

    quotes gen

--- tweetbot.py
+++ tweetbot.py
@@ -1,5 +1,7 @@
 import tweepy
+from swquotes import randomq
 import time
+
 
 app_key = 'rCguhoSNkQGrj2WfowC0iI4rj'
 app_secret = 'nMU6MarVejiIpTPCx7aYrHwuiawbRywh13lXhXOuymbz77BieZ'
@@ -11,19 +13,3 @@ auth = tweepy.OAuthHandler(app_key, app_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
-hashtag = ["#Sith", "StarWars", "Lightsabers"]
-tweetCount = 20
-
-tweets = tweepy.Cursor(api.search, hashtag).items(tweetCount)
-
-def searchBot():
-    for tweet in tweets:
-        try:
-            tweet.retweet()
-            api.create_favorite(tweet.id)
-            time.sleep(2)
-        except tweepy.TweepError as e:
-            print(e.reason)
-            time.sleep(2)
-searchBot()
